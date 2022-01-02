25f75b5f7bed90ffc42ef1f206dff2d2a393e8e7
hawei <13597213421@163.com>
2022-01-02 20:11:40 +0800

    update jenkis file * export the inline report

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,6 @@ npx cypress run
 echo "merge test report..... -> npx mochawesome-merge cypress/results/*.json > merge-report.json"
 npx mochawesome-merge cypress/results/*.json > merge-report.json
 
-echo "generator test report..... -> npx mochawesome-report-generator -o=./report merge-report.json"
-npx mochawesome-report-generator -o=./report merge-report.json
+echo "generator test report..... -> npx mochawesome-report-generator -o=./report -i merge-report.json"
+npx mochawesome-report-generator -o=./report -i merge-report.json
 
